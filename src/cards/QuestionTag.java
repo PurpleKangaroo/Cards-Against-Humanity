8563c8c6cb07d0566de82951c42dfeb46c264bb1
HoltMaki <holtmaki@gmail.com>
2013-12-05 19:12:07 -0800

    tags coments added

---
+++ src/cards/QuestionTag.java
@@ -0,0 +1,44 @@
+package cards;
+
+/**
+ * An enum that contains all of the QuestionCards' tags.
+ * Tags are important for helping the computer impersonate people as well as learn what is funny.
+ * @author Devon Grove
+ * @since CAH1.0
+ * @version CAH1.0
+ *
+ */
+public enum QuestionTag 
+{
+	//I have no idea how we should do this.a
+		//IDEAS:
+		/*Should we separate it into large categorys like racial, sexual, etc.?
+		 * Should we have some way to have subcategorys?
+		 * Or should the tags just be like RACIAL_BLACK
+		 * I have no idea about any of this these are just ideas
+		 * 
+		 * Should they include things like EVENT_TRAGIC (this would be applied to famous events like 9/11), 
+		 * CELEBRETY_ENTERTAINMENT_MOVIES, CELEBRETY_POLITICAL_CONSERVATIVE, CELEBRITY_HISTORICAL_RUTHLESSDICTATOR,etc.
+		 * 
+		 * Or should there be a class, subject that contains multiple enums that represent the subject of the card? For example if
+		 * the card was about a person it could say if the person was a man or a woman, etc because that might affect the demographic 
+		 * that likes the card
+		 * 
+		 * NOTE: There is no inheritance with enums
+		 * NOTE: Enums are written as follows
+		 * RACIAL, <-------- All caps, then comma
+		 * ...... <--------- These would be more options
+		 * ...... <------------------|
+		 * ...... <------------------|
+		 * ...... <------------------|
+		 * ...... <------------------+
+		 * SDFDSFDS; <---------END THE ENUM WITH A SEMICOLON
+		 * More info at: http://docs.oracle.com/javase/tutorial/java/javaOO/enum.html
+		 */
+		
+		//Another idea - consolodate the AnswerTag and QuestionTag into one enum that would contain all of them.
+	}
+
+
+
+
