ce379f0cdea50208f35e9166663ae770d9f58e69
HoltMaki <holtmaki@gmail.com>
2013-12-05 18:47:09 -0800

    tags changes

--- src/cards/tag.java
+++ src/cards/tag.java
@@ -1,13 +1,15 @@
 package cards;
 /**
- * Creates an object that acts as a tag for cards
+ * An enum that 
  * Tags are important for helping the computer impersonate people as well as learn what is funny.
- * This class should use the aTagsEnum.
  * @author Holt Maki
- * @since incompete
- * @version incomplete
+ * @since CAH1.0
+ * @version CAH1.0
  *
  */
-public class Tag {
-
+public enum Tag
+{
+	
 }
+
+
