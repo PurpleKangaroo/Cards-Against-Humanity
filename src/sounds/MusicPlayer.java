2916d89065ec8cea99232a44231e759fc057d24c
HoltMaki <holtmaki@gmail.com>
2014-01-07 12:25:27 -0800

    music player documentation

--- src/sounds/MusicPlayer.java
+++ src/sounds/MusicPlayer.java
@@ -6,12 +6,26 @@ import java.util.TreeMap;
 
 import javax.print.attribute.standard.Media;
 
+/**
+ * A class of object that plays music.
+ * @author Holt Maki
+ * @since CAH1.0
+ * @version CAH1.0
+ */
 public class MusicPlayer implements Runnable 
 {
 	//TODO use sliders for music and sound effects volume.
 	private Map<String, SongInfo> allSongs;
 	private Map<String, SongInfo> selectedSongs;
 	
+	/**
+	 * Creates a new object that plays music.
+	 * This music constructor is blank because it simply initializes the TreeMap.
+	 * Songs are added after the creation of the object if this constructor is used.
+	 * @since CAH1.0
+	 * @version CAH1.0
+	 * @see {@linkplain #MusicPlayer(TreeMap)} for a constructor that 
+	 */
 	public MusicPlayer()
 	{
 		allSongs = new TreeMap<String, SongInfo>();
@@ -23,6 +37,11 @@ public class MusicPlayer implements Runnable
 		allSongs = songmap;
 	}
 	
+	/**
+	 * Adds a song to the MusicPlayer
+	 * @param pathname
+	 * @param info
+	 */
 	protected void addSong(String pathname, SongInfo info)
 	{
 		allSongs.put(pathname, info);
