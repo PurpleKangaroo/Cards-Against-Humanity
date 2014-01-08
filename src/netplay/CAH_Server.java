947a8d992b3bd182b0ad282cf48ccea94a9e10ac
HoltMaki <holtmaki@gmail.com>
2014-01-07 17:43:31 -0800

    work on server

--- src/netplay/CAH_Server.java
+++ src/netplay/CAH_Server.java
@@ -9,13 +9,37 @@ import java.net.*;
  * @since CAH1.0
  * @version CAH1.0
  * <dt></dt>
- * <b>Implements:</b> <dl> {@linkplain Runnable} </dl>
+ * <b>Extends:</b> <dl> {@linkplain Thread} </dl>
  */
-public class CAH_Server implements Runnable {
+public class CAH_Server extends Thread {
+	private static int port = 2000;
+	private ServerSocket serverSocket;
 	
+	/**
+	 * Creates an object that represents a Cards Against Humanity Server
+	 * @since CAH1.0
+	 * @version CAH1.0
+	 */
 	public CAH_Server()
-	{
+	{		
+		boolean socketCreated = false;
 		
+		while(!socketCreated)
+		{
+			
+			port++;
+			
+			try
+			{
+				serverSocket = new ServerSocket(port);
+			}
+			
+			catch(IOException e)
+			{
+				
+			}
+			
+		}
 	}
 	
 	public void run()
