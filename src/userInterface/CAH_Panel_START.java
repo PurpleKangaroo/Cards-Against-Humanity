facaf336baf626fbaaa75ef3cc53a5cafb4d7b8e
HoltMaki <holtmaki@gmail.com>
2014-01-09 13:29:04 -0800

    gov

--- src/userInterface/CAH_Panel_START.java
+++ src/userInterface/CAH_Panel_START.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import game.*;
 
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 
 import users.Player;
 import cards.DeckBuilder;
@@ -18,15 +19,28 @@ import cards.DeckBuilder;
  * @version CAH1.0
  *
  */
+@SuppressWarnings("serial")
 public class CAH_Panel_START extends JPanel 
 {
+	//TODO add tool tips for the tools, especially for when someone has an invalid set of rules.
+	/**The slider that is used to choose how many players with each player chosen
+	 *  a new bordered area with swing tools that allow player setup will be added.
+	 *  This bordered area will include information such as if the player is a computer,
+	 *  and maybe their username or IP. (maybe we can have them send that information automatically to the
+	 *  computer which will display it when the user connects.*/
+	private JSlider numberOfPlayerSlider;
 	/**A list of the players playing in the game*/
 	private ArrayList<Player> players;
 	/**The list of house rules that the game is using.*/
 	private ArrayList<HouseRules> houseRules;
 	
+	/**
+	 * Creates an object that represents the start screen for Cards Against Humanity.
+	 */
 	public CAH_Panel_START()
 	{
+		super();
+		
 		players = new ArrayList<Player>();
 		houseRules = new ArrayList<HouseRules>();
 	}
