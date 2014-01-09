aee267b9a24336e250e0dd2623d6aafbf5e3baa3
HoltMaki <holtmaki@gmail.com>
2014-01-08 16:10:46 -0800

    documentation

--- src/userInterface/package-info.java
+++ src/userInterface/package-info.java
@@ -1,6 +1,10 @@
 
 /**
- * @author 
+ * A package that contains classes that have to do with creating and running the UI.
+ * @author Nick Walker
+ * @author Holt Maki
+ * @since CAH1.0
+ * @version CAH1.0
  *
  */
 package userInterface;
