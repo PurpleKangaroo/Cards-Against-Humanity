1dff41d0bf10f6d9a37fd268087930b9f5b13ac1
HoltMaki <holtmaki@gmail.com>
2014-02-19 14:49:55 -0800

    some dialogs added

---
+++ src/ui/AddComputerPlayer.java
@@ -0,0 +1,102 @@
+package ui;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.BoxLayout;
+import javax.swing.JSplitPane;
+import javax.swing.JTextPane;
+import javax.swing.UIManager;
+import javax.swing.JLabel;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class AddComputerPlayer extends JDialog
+{
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args)
+	{
+		try
+		{
+			AddComputerPlayer dialog = new AddComputerPlayer();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public AddComputerPlayer()
+	{
+		addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				//TODO: left and right key move each way through the computer player list
+			}
+		});
+		setTitle("Add Computer Player");
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.X_AXIS));
+		{
+			JPanel computerInfoPanel = new JPanel();
+			contentPanel.add(computerInfoPanel);
+			computerInfoPanel.setLayout(new BoxLayout(computerInfoPanel, BoxLayout.Y_AXIS));
+			{
+				JPanel infoPanel = new JPanel();
+				computerInfoPanel.add(infoPanel);
+				infoPanel.setLayout(null);
+				{
+					JLabel lblName = new JLabel("Name:");
+					lblName.setBounds(10, 11, 192, 14);
+					infoPanel.add(lblName);
+				}
+				{
+					JLabel lblDifficulty = new JLabel("Difficulty:");
+					lblDifficulty.setBounds(10, 27, 192, 14);
+					infoPanel.add(lblDifficulty);
+				}
+			}
+			{
+				JPanel statsPanel = new JPanel();
+				computerInfoPanel.add(statsPanel);
+			}
+		}
+		{
+			JPanel computerPicturePanel = new JPanel();
+			contentPanel.add(computerPicturePanel);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+}
---
+++ src/ui/AddPlayer.java
@@ -0,0 +1,156 @@
+package ui;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextPane;
+import java.awt.Color;
+import javax.swing.UIManager;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class AddPlayer extends JDialog
+{
+
+	private final JPanel contentPanel = new JPanel();
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+	/**
+	 * Tells weather or not the user chose a human player to be added.
+	 */
+	private boolean human;
+	/**
+	 * Tells weather or not the user chose a computer player to be added.
+	 */
+	private boolean computer;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args)
+	{
+		try
+		{
+			AddPlayer dialog = new AddPlayer();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public AddPlayer()
+	{
+		setUndecorated(true);
+		setResizable(false);
+		setBounds(100, 100, 247, 162);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JTextPane txtpnDoYouWish = new JTextPane();
+			txtpnDoYouWish.setBounds(10, 10, 211, 47);
+			txtpnDoYouWish.setBackground(UIManager.getColor("Panel.background"));
+			txtpnDoYouWish.setDisabledTextColor(Color.BLACK);
+			txtpnDoYouWish.setEnabled(false);
+			txtpnDoYouWish.setEditable(false);
+			txtpnDoYouWish.setFocusable(false);
+			txtpnDoYouWish.setFocusCycleRoot(false);
+			txtpnDoYouWish.setFocusTraversalKeysEnabled(false);
+			txtpnDoYouWish.setText("Do you wish to add a computer player or a human player?");
+			contentPanel.add(txtpnDoYouWish);
+		}
+			JPanel panel = new JPanel();
+			panel.setBounds(10, 49, 211, 66);
+			contentPanel.add(panel);
+			
+			final JRadioButton rdbtnComputerPlayer = new JRadioButton("Computer Player");
+			rdbtnComputerPlayer.setFocusTraversalKeysEnabled(false);
+			rdbtnComputerPlayer.setFocusPainted(false);
+			rdbtnComputerPlayer.setSelected(true);
+			buttonGroup.add(rdbtnComputerPlayer);
+			panel.add(rdbtnComputerPlayer);
+			{
+				JRadioButton rdbtnHumanPlayer = new JRadioButton("Human Player");
+				rdbtnHumanPlayer.setFocusTraversalKeysEnabled(false);
+				rdbtnHumanPlayer.setFocusPainted(false);
+				buttonGroup.add(rdbtnHumanPlayer);
+				panel.add(rdbtnHumanPlayer);
+			}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						if(rdbtnComputerPlayer.isSelected())
+						{
+							computer = true;
+							human = false;
+						}
+						else
+						{
+							human = true;
+							computer = false;
+						}
+						setVisible(false);
+					}
+				});
+				okButton.setFocusPainted(false);
+				okButton.setFocusTraversalKeysEnabled(false);
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener(){
+
+					@Override
+					public void actionPerformed(ActionEvent e)
+					{
+						setVisible(false);
+						
+					}
+					
+				});
+				cancelButton.setFocusPainted(false);
+				cancelButton.setFocusTraversalKeysEnabled(false);
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+	
+	/**
+	 * Gets the boolean telling weather or not the player being added is a human player.
+	 * @return human - the boolean telling weather or not the player being added is a human player.
+	 * @since CAH1.0
+	 */
+	protected boolean getHuman()
+	{
+		return human;
+	}
+	
+	/**
+	 * Gets the boolean telling weather or not the player being added is a computer player.
+	 * @return computer - the boolean telling weather or not the player being added is a computer player.
+	 * @since CAH1.0
+	 */
+	protected boolean getComputer()
+	{
+		return computer;
+	}
+}
