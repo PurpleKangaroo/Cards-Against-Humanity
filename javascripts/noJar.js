bf5f7d7ebd701191071724d9b099246bb0cab7da
Holt Maki <holtmaki@gmail.com>
2014-02-19 17:08:08 -0800

    Update noJar.js

--- javascripts/noJar.js
+++ javascripts/noJar.js
@@ -2,5 +2,5 @@ $(document).ready(function() {
   $('#noJar').click(function (event) {
     event.preventDefault();
     alert("Sorry! We have not released Cards Against Humanity yet. Check back soon.");
-  }
+  });
 });
