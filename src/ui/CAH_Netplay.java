527df8fe857f985291231c928e003783ae73df9b
HoltMaki <holtmaki@gmail.com>
2014-02-19 17:40:11 -0800

    added a bit of doc

--- src/ui/CAH_Netplay.java
+++ src/ui/CAH_Netplay.java
@@ -2,6 +2,13 @@ package ui;
 
 import javax.swing.JPanel;
 
+/**
+ * A class of object that represents the panel that will come up to allow a player to start up netplay.
+ * @author Holt Maki
+ * @since CAH1.0
+ * @version CAH1.0
+ *
+ */
 public class CAH_Netplay extends JPanel
 {
 
