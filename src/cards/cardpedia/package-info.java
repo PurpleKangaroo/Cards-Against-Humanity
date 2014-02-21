6d60dc5a75bee60dddfc7a1f83328253132bf960
HoltMaki <holtmaki@gmail.com>
2014-02-21 08:40:39 -0800

    updated cardpedia package info

--- src/cards/cardpedia/package-info.java
+++ src/cards/cardpedia/package-info.java
@@ -5,9 +5,8 @@
  * The package that deals with the cardpedia, which will contain information about each card. This will
  * be important in a game with the rule "Never Have I Ever," where a pop-up will come up when a player
  * discards a card that they dont understand.
+ * Hope to get this done by CAH
  * @author Holt Maki
- * @since CAH1.0
- * @version CAH1.0
  *
  */
 package cards.cardpedia;
