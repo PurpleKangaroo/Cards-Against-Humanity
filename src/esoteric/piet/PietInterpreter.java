437531add4c65c1224c07d199a576d8ed1a07845
HoltMaki <holtmaki@gmail.com>
2014-03-11 20:26:02 -0700

    Interpreter work

--- src/esoteric/piet/PietInterpreter.java
+++ src/esoteric/piet/PietInterpreter.java
@@ -32,7 +32,64 @@ public class PietInterpreter
 		{
 			while(true)
 			{
+				incrementLocation();
 				Command command = getCommand();
+				switch(command)
+				{
+				case PUSH:
+					//TODO fill
+					break;
+				case POP:
+					//TODO fill
+					break;
+				case ADD:
+					//TODO fill
+					break;
+				case SUBTRACT:
+					//TODO fill
+					break;
+				case MULTIPLY:
+					//TODO fill
+					break;
+				case DEVIDE:
+					//TODO fill
+					break;
+				case MOD:
+					//TODO fill
+					break;
+				case NOT:
+					//TODO fill
+					break;
+				case GREATER:
+					//TODO fill
+					break;
+				case POINTER:
+					//TODO fill
+					break;
+				case SWITCH:
+					//TODO fill
+					break;
+				case DUPLICATE:
+					//TODO fill
+					break;
+				case ROLL:
+					//TODO fill
+					break;
+				case IN_NUMBER:
+					//TODO fill
+					break;
+				case IN_CHAR:
+					//TODO fill
+					break;
+				case OUT_NUMBER:
+					//TODO fill
+					break;
+				case OUT_CHAR:
+					//TODO fill
+					break;
+				default:
+					throw new Command.InvalidCommandError();
+				}
 			}
 		}
 		catch(BlackColorException e)
@@ -118,21 +175,47 @@ public class PietInterpreter
 				}
 				if(chooser.equals(CodelChooser.RIGHT))
 				{
-					while(sameColor(-1 * codelSize, 1))
+					while(sameColor(-1 * codelSize, -1))
 					{
 						location  = new Point((int) location.getX() - codelSize, (int) location.getY());
 					}
 				}
 				else
 				{
-					while(sameColor(codelSize, 1))
+					while(sameColor(codelSize, -1))
 					{
 						location  = new Point((int) location.getX() + codelSize, (int) location.getY());
 					}
 				}
 				
 			}
-			while(sameColor(0, codelSize) || (sameColor(codelSize, 1) && chooser.equals(CodelChooser.LEFT)) || (sameColor(-1 * codelSize, 1) && chooser.equals(CodelChooser.LEFT)));		
+			while(sameColor(0, codelSize) || (sameColor(codelSize, -1) && chooser.equals(CodelChooser.LEFT)) || (sameColor(-1 * codelSize, -1) && chooser.equals(CodelChooser.LEFT)));		
+		}
+		else if(direction.equals(DirectionPointer.UP))
+		{
+			do
+			{
+				while(sameColor(0, -1 * codelSize))
+				{
+					location  = new Point((int) location.getX(), (int) location.getY() - codelSize);			
+				}
+				if(chooser.equals(CodelChooser.RIGHT))
+				{
+					while(sameColor(codelSize, 1))
+					{
+						location  = new Point((int) location.getX() + codelSize, (int) location.getY());
+					}
+				}
+				else
+				{
+					while(sameColor(-1 * codelSize, 1))
+					{
+						location  = new Point((int) location.getX() - codelSize, (int) location.getY());
+					}
+				}
+				
+			}
+			while(sameColor(0, codelSize) || (sameColor(-1 * codelSize, 1) && chooser.equals(CodelChooser.LEFT)) || (sameColor(codelSize, 1) && chooser.equals(CodelChooser.LEFT)));		
 		}
 	}
 	
