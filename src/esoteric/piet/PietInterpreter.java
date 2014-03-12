91d4c984a56ec2907b1f9606063c27750709332c
HoltMaki <holtmaki@gmail.com>
2014-03-11 19:54:48 -0700

    Working on piet interpreter

--- src/esoteric/piet/PietInterpreter.java
+++ src/esoteric/piet/PietInterpreter.java
@@ -1,18 +1,14 @@
 package esoteric.piet;
 
-import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 import java.util.Stack;
-
-import javax.imageio.ImageIO;
 
 import esoteric.piet.PietColor.BlackColorException;
 
 public class PietInterpreter
 {
-	private Dimension codelSize;
+	private int codelSize;
 	private BufferedImage code;
 	private Point location;
 	private DirectionPointer direction;
@@ -20,12 +16,14 @@ public class PietInterpreter
 	private PietColor lastColor;
 	private Stack<Byte> pietStack;
 	
-	public PietInterpreter(BufferedImage code, Dimension codelSize)
+	public PietInterpreter(BufferedImage code, int codelSize)
 	{
 		this.code = code;
 		location = new Point(0, 0);
 		pietStack = new Stack<Byte>();
 		this.codelSize = codelSize;
+		chooser = CodelChooser.LEFT;
+		direction = DirectionPointer.RIGHT;
 	}
 	
 	public String interpretToText()
@@ -46,12 +44,101 @@ public class PietInterpreter
 	
 	private Command getCommand()
 	{
-		return Command.getCommand(this.lastColor, PietColor.getPietColor(code.getRGB((int) location.getX(), (int) location.getY())));
+		return Command.getCommand(this.lastColor, getPietColor());
+	}
+	
+	private PietColor getPietColor()
+	{
+		return PietColor.getPietColor(code.getRGB((int) location.getX(), (int) location.getY()));
 	}
 	
 	private void incrementLocation()
 	{
+		lastColor = getPietColor();
 		
+		if(direction.equals(DirectionPointer.LEFT))
+		{
+			do
+			{
+				while(sameColor(-1 * codelSize, 0))
+				{
+					location  = new Point((int) location.getX() - codelSize, (int) location.getY());			
+				}
+				if(chooser.equals(CodelChooser.LEFT))
+				{
+					while(sameColor(1, codelSize))
+					{
+						location  = new Point((int) location.getX(), (int) location.getY() + codelSize);
+					}
+				}
+				else
+				{
+					while(sameColor(1, -1 * codelSize))
+					{
+						location  = new Point((int) location.getX(), (int) location.getY() - codelSize);
+					}
+				}
+				
+			}
+			while(sameColor(-1 * codelSize, 0) || (sameColor(1, codelSize) && chooser.equals(CodelChooser.LEFT)) || (sameColor(1, -1 * codelSize) && chooser.equals(CodelChooser.LEFT)));		
+		}
+		else if(direction.equals(DirectionPointer.RIGHT))
+		{
+			do
+			{
+				while(sameColor(codelSize, 0))
+				{
+					location  = new Point((int) location.getX() + codelSize, (int) location.getY());			
+				}
+				if(chooser.equals(CodelChooser.RIGHT))
+				{
+					while(sameColor(-1, codelSize))
+					{
+						location  = new Point((int) location.getX(), (int) location.getY() + codelSize);
+					}
+				}
+				else
+				{
+					while(sameColor(-1, -1 * codelSize))
+					{
+						location  = new Point((int) location.getX(), (int) location.getY() - codelSize);
+					}
+				}
+				
+			}
+			while(sameColor(codelSize, 0) || (sameColor(-1, -1 * codelSize) && chooser.equals(CodelChooser.LEFT)) || (sameColor(-1, codelSize) && chooser.equals(CodelChooser.LEFT)));		
+		}
+		else if(direction.equals(DirectionPointer.DOWN))
+		{
+			do
+			{
+				while(sameColor(0, codelSize))
+				{
+					location  = new Point((int) location.getX(), (int) location.getY() + codelSize);			
+				}
+				if(chooser.equals(CodelChooser.RIGHT))
+				{
+					while(sameColor(-1 * codelSize, 1))
+					{
+						location  = new Point((int) location.getX() - codelSize, (int) location.getY());
+					}
+				}
+				else
+				{
+					while(sameColor(codelSize, 1))
+					{
+						location  = new Point((int) location.getX() + codelSize, (int) location.getY());
+					}
+				}
+				
+			}
+			while(sameColor(0, codelSize) || (sameColor(codelSize, 1) && chooser.equals(CodelChooser.LEFT)) || (sameColor(-1 * codelSize, 1) && chooser.equals(CodelChooser.LEFT)));		
+		}
+	}
+	
+	private boolean sameColor(int xIncr, int yIncr)
+	{
+		return lastColor.equals(PietColor.getPietColor(code.getRGB((int) location.getX() + xIncr, (int) location.getY() + yIncr))) && (location.getX() + xIncr) < code.getWidth() && (location.getY() + yIncr) < code.getHeight();
 	}
 	
 }
