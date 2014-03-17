d5a674f908831c83f4e4d9ca77f6f1a43e66c188
HoltMaki <holtmaki@gmail.com>
2014-03-17 08:19:56 -0700

    Javadoc

--- src/esoteric/brainfuck/Brainfuck.java
+++ src/esoteric/brainfuck/Brainfuck.java
@@ -1,15 +1,39 @@
 package esoteric.brainfuck;
 
+/**
+ * The class that does all Brainfuck operations, using other {@linkplain esoteric.brainfuck} classes. Consolidates all Brainfuck operations.
+ * @author Holt Maki
+ * @since CAH1.0
+ * @version CAH1.0
+ * @see BrainfuckInterpreter
+ * @see TextToBrainfuck
+ *
+ */
 public class Brainfuck
 {
+	/**
+	 * Translates a String of normal text into Brainfuck.
+	 * @author Holt Maki
+	 * @param text The text that is being put into Brainfuck.
+	 * @return {@code TextToBrainfuck.brainFuck(text)} - A String of Brainfuck, translated from text.
+	 * @since CAH1.0
+	 * @see TextToBrainfuck
+	 * @see TextToBrainfuck#TextToBrainfuck()
+	 */
 	public static String toBrainfuck(String text)
 	{
 		return TextToBrainfuck.brainFuck(text);
 	}
 	
+	/**
+	 * Translates a String of Brainfuck into normal text.
+	 * @param brainfuck The String of Brainfuck being translated into normal text.
+	 * @author Holt Maki
+	 * @return {@code (new BrainfuckInterpreter()).interpret(brainfuck)} - A string of normal text interpreted from Brainfuck.
+	 * @since CAH1.0
+	 */
 	public static String toText(String brainfuck)
 	{
-		BrainfuckInterpreter a = new BrainfuckInterpreter();
-		return a.interpret(brainfuck);
+		return (new BrainfuckInterpreter()).interpret(brainfuck);
 	}
 }
--- src/esoteric/brainfuck/package-info.java
+++ src/esoteric/brainfuck/package-info.java
@@ -1,10 +1,9 @@
 /**
- * 
- */
-/**
+ * The package that works with the Brainfuck esoteric programming language.
  * @author Holt Maki
  * @since CAH1.0
- * @version cAH1.0
+ * @version CAH1.0
+ * @see <a href="http://en.wikipedia.org/wiki/Brainfuck">Wikipedia: Brainfuck</a>
  *
  */
 package esoteric.brainfuck;
