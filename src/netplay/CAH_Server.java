ccfa41b8e760d55e77fc99ed6bdf7fe1e16c1297
HoltMaki <holtmaki@gmail.com>
2014-03-25 11:08:22 -0700

    worked on CAH_Server

--- src/netplay/CAH_Server.java
+++ src/netplay/CAH_Server.java
@@ -1,6 +1,11 @@
 package netplay;
 
-import java.util.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.Socket;
+
+import javax.swing.Timer;
 
 import com.esotericsoftware.kryonet.Server;
 
@@ -9,24 +14,47 @@ public class CAH_Server implements Runnable
 	private Server gameServer;
 	private Timer updateTimer;
 	private ConnectionType connectionType;
+	private Socket localHost;//TODO connect this with a local client that then controls the UI
 	
 	public CAH_Server(ConnectionType connectionType)
 	{
 		gameServer = new Server();
 		this.connectionType = connectionType;
+		updateTimer = new Timer(100, new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0)
+			{
+				try
+				{
+					gameServer.update(10);
+				} catch (IOException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		});
 		
 	}
 
 	@Override
 	public void run()
 	{
-		/*TODO have timer go off at a certain interval, then when it does call gameServer.update();
-		TODO then send everything from the server to "localhost" client, as well as all of the clients that aren't connected.
-		TODO Gamelogic all run on server in netplay...
-		TODO UI all on client.
-		TODO Leave as much Gamelogic off of the clients as possible
-		TODO have the server decide based on UDP/TCP based on onnection type
-		*/
+		boolean running = true;
+		while(running)
+		{
+			
+		}
+		/*
+		 * TODO have timer go off at a certain interval, then when it does call gameServer.update();
+		 * TODO then send everything from the server to "localhost" client, as well as all of the clients that aren't connected.
+		 * TODO Gamelogic all run on server in netplay...
+		 * TODO UI all on client.
+		 * TODO Leave as much Gamelogic off of the clients as possible
+		 * TODO have the server decide based on UDP/TCP based on onnection type
+		 */
 	}
 	
 	enum ConnectionType
